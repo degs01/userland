3dc95fd68a62e6e2dceadd395ae313c55b2fe9c5
Warg <warg@is-la.me>
2012-10-27 23:17:05 +0200

    Adding prebuilt modules, could be wonky but could also work fine.

---
+++ build/arm-android/release/CMakeFiles/CMakeCXXCompiler.cmake
@@ -0,0 +1,54 @@
+SET(CMAKE_CXX_COMPILER "/home/viktor/arm-linux-androideabi-4.6.3/bin/arm-linux-androideabi-g++")
+SET(CMAKE_CXX_COMPILER_ARG1 "")
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+SET(CMAKE_CXX_COMPILER_VERSION "4.6.3")
+SET(CMAKE_CXX_PLATFORM_ID "Linux")
+
+SET(CMAKE_AR "/home/viktor/arm-linux-androideabi-4.6.3/bin/arm-linux-androideabi-ar")
+SET(CMAKE_RANLIB "/home/viktor/arm-linux-androideabi-4.6.3/bin/arm-linux-androideabi-ranlib")
+SET(CMAKE_LINKER "/home/viktor/arm-linux-androideabi-4.6.3/bin/arm-linux-androideabi-ld")
+SET(CMAKE_COMPILER_IS_GNUCXX 1)
+SET(CMAKE_CXX_COMPILER_LOADED 1)
+SET(CMAKE_COMPILER_IS_MINGW )
+SET(CMAKE_COMPILER_IS_CYGWIN )
+IF(CMAKE_COMPILER_IS_CYGWIN)
+  SET(CYGWIN 1)
+  SET(UNIX 1)
+ENDIF(CMAKE_COMPILER_IS_CYGWIN)
+
+SET(CMAKE_CXX_COMPILER_ENV_VAR "CXX")
+
+IF(CMAKE_COMPILER_IS_MINGW)
+  SET(MINGW 1)
+ENDIF(CMAKE_COMPILER_IS_MINGW)
+SET(CMAKE_CXX_COMPILER_ID_RUN 1)
+SET(CMAKE_CXX_IGNORE_EXTENSIONS inl;h;hpp;HPP;H;o;O;obj;OBJ;def;DEF;rc;RC)
+SET(CMAKE_CXX_SOURCE_FILE_EXTENSIONS C;M;c++;cc;cpp;cxx;m;mm;CPP)
+SET(CMAKE_CXX_LINKER_PREFERENCE 30)
+SET(CMAKE_CXX_LINKER_PREFERENCE_PROPAGATES 1)
+
+# Save compiler ABI information.
+SET(CMAKE_CXX_SIZEOF_DATA_PTR "4")
+SET(CMAKE_CXX_COMPILER_ABI "ELF")
+SET(CMAKE_CXX_LIBRARY_ARCHITECTURE "")
+
+IF(CMAKE_CXX_SIZEOF_DATA_PTR)
+  SET(CMAKE_SIZEOF_VOID_P "${CMAKE_CXX_SIZEOF_DATA_PTR}")
+ENDIF(CMAKE_CXX_SIZEOF_DATA_PTR)
+
+IF(CMAKE_CXX_COMPILER_ABI)
+  SET(CMAKE_INTERNAL_PLATFORM_ABI "${CMAKE_CXX_COMPILER_ABI}")
+ENDIF(CMAKE_CXX_COMPILER_ABI)
+
+IF(CMAKE_CXX_LIBRARY_ARCHITECTURE)
+  SET(CMAKE_LIBRARY_ARCHITECTURE "")
+ENDIF()
+
+SET(CMAKE_CXX_HAS_ISYSROOT "")
+
+
+SET(CMAKE_CXX_IMPLICIT_LINK_LIBRARIES "stdc++;m;crystax;stdc++;log;c;dl")
+SET(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "/home/viktor/arm-linux-androideabi-4.6.3/lib/gcc/arm-linux-androideabi/4.6.3;/home/viktor/arm-linux-androideabi-4.6.3/lib/gcc;/home/viktor/arm-linux-androideabi-4.6.3/arm-linux-androideabi/lib;/home/viktor/arm-linux-androideabi-4.6.3/sysroot/usr/lib")
+
+
+
---
+++ build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/DependInfo.cmake
@@ -0,0 +1,95 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/viktor/userland/interface/khronos/common/khrn_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/khrn_client.c.o"
+  "/home/viktor/userland/interface/khronos/common/khrn_client_cache.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/khrn_client_cache.c.o"
+  "/home/viktor/userland/interface/khronos/common/khrn_client_global_image_map.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/khrn_client_global_image_map.c.o"
+  "/home/viktor/userland/interface/khronos/common/khrn_client_pointermap.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/khrn_client_pointermap.c.o"
+  "/home/viktor/userland/interface/khronos/common/khrn_client_vector.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/khrn_client_vector.c.o"
+  "/home/viktor/userland/interface/khronos/common/khrn_int_hash.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/khrn_int_hash.c.o"
+  "/home/viktor/userland/interface/khronos/common/khrn_int_image.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/khrn_int_image.c.o"
+  "/home/viktor/userland/interface/khronos/common/khrn_int_util.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/khrn_int_util.c.o"
+  "/home/viktor/userland/interface/khronos/common/khrn_options.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/khrn_options.c.o"
+  "/home/viktor/userland/interface/khronos/common/linux/khrn_client_platform_linux.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/linux/khrn_client_platform_linux.c.o"
+  "/home/viktor/userland/interface/khronos/common/linux/khrn_client_rpc_linux.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/linux/khrn_client_rpc_linux.c.o"
+  "/home/viktor/userland/interface/khronos/common/openwfc/khrn_client_platform_openwfc.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/common/openwfc/khrn_client_platform_openwfc.c.o"
+  "/home/viktor/userland/interface/khronos/egl/egl_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/egl/egl_client.c.o"
+  "/home/viktor/userland/interface/khronos/egl/egl_client_config.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/egl/egl_client_config.c.o"
+  "/home/viktor/userland/interface/khronos/egl/egl_client_context.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/egl/egl_client_context.c.o"
+  "/home/viktor/userland/interface/khronos/egl/egl_client_get_proc.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/egl/egl_client_get_proc.c.o"
+  "/home/viktor/userland/interface/khronos/egl/egl_client_surface.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/egl/egl_client_surface.c.o"
+  "/home/viktor/userland/interface/khronos/ext/egl_brcm_driver_monitor_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/egl_brcm_driver_monitor_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/egl_brcm_flush_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/egl_brcm_flush_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/egl_brcm_global_image_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/egl_brcm_global_image_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/egl_brcm_perf_monitor_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/egl_brcm_perf_monitor_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/egl_khr_image_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/egl_khr_image_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/egl_khr_lock_surface_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/egl_khr_lock_surface_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/egl_khr_sync_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/egl_khr_sync_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/egl_openmaxil_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/egl_openmaxil_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/ext_gl_debug_marker.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/ext_gl_debug_marker.c.o"
+  "/home/viktor/userland/interface/khronos/ext/gl_oes_draw_texture_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/gl_oes_draw_texture_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/gl_oes_egl_image_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/gl_oes_egl_image_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/gl_oes_framebuffer_object.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/gl_oes_framebuffer_object.c.o"
+  "/home/viktor/userland/interface/khronos/ext/gl_oes_map_buffer.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/gl_oes_map_buffer.c.o"
+  "/home/viktor/userland/interface/khronos/ext/gl_oes_matrix_palette_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/gl_oes_matrix_palette_client.c.o"
+  "/home/viktor/userland/interface/khronos/ext/gl_oes_query_matrix_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/ext/gl_oes_query_matrix_client.c.o"
+  "/home/viktor/userland/interface/khronos/glxx/glxx_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/glxx/glxx_client.c.o"
+  "/home/viktor/userland/interface/khronos/vg/vg_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/vg/vg_client.c.o"
+  "/home/viktor/userland/interface/khronos/vg/vg_int_mat3x3.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/vg/vg_int_mat3x3.c.o"
+  "/home/viktor/userland/interface/khronos/wf/wfc_client.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/wf/wfc_client.c.o"
+  "/home/viktor/userland/interface/khronos/wf/wfc_client_ipc.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/wf/wfc_client_ipc.c.o"
+  "/home/viktor/userland/interface/khronos/wf/wfc_client_server_api.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/wf/wfc_client_server_api.c.o"
+  "/home/viktor/userland/interface/khronos/wf/wfc_client_stream.c" "/home/viktor/userland/build/arm-android/release/interface/khronos/CMakeFiles/khrn_static.dir/wf/wfc_client_stream.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_HAVE_SBRK"
+  "_LARGEFILE64_SOURCE"
+  "HAVE_CMAKE_CONFIG"
+  "HAVE_VMCS_CONFIG"
+  "_REENTRANT"
+  "USE_VCHIQ_ARM"
+  "VCHI_BULK_ALIGN=1"
+  "VCHI_BULK_GRANULARITY=1"
+  "OMX_SKIP64BIT"
+  "EGL_SERVER_DISPMANX"
+  "_LARGEFILE_SOURCE"
+  "_LARGEFILE64_SOURCE"
+  "__VIDEOCORE4__"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../inc"
+  "/home/viktor/cm/prebuilt/ndk/android-ndk-r/platforms/android-/arch-arm/usr/include"
+  "../../../BEFORE"
+  "/home/viktor/cm/bionic/libc/include/arch-arm/include"
+  "/home/viktor/cm/bionic/libc/include"
+  "/home/viktor/cm/bionic/libc/kernel/arch-arm"
+  "/home/viktor/cm/bionic/libc/kernel/common"
+  "/home/viktor/cm/bionic/libm/include"
+  "/home/viktor/cm/bionic/libm/include/arch/arm"
+  "/home/viktor/cm/bionic/libstdc++/include"
+  "../../../host_applications/framework"
+  "../../.."
+  "../../../interface/vcos/pthreads"
+  "../../../interface/vmcs_host/linux"
+  "../../../interface/vmcs_host"
+  "../../../interface/vmcs_host/khronos"
+  "../../../interface/khronos/include"
+  "."
+  "../../../interface/vchiq_arm"
+  "../../../host_support/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
