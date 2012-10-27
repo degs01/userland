a836b1b8ab1355aa4eebfb77ea29543baefaa5f6
Warg <warg@is-la.me>
2012-10-27 13:22:41 +0200

    Initial Android support for building added

---
+++ makefiles/cmake/toolchains/arm-android.cmake
@@ -0,0 +1,24 @@
+#
+# CMake defines to cross-compile to ARM/Linux on BCM2708 using glibc.
+#
+
+
+# Note that the compilers should be in your $PATH
+SET(CMAKE_SYSTEM_NAME Android)
+SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+SET(CMAKE_ASM_COMPILER arm-linux-gnueabihf-as)
+SET(CMAKE_SYSTEM_PROCESSOR arm)
+SET(ANDROID TRUE)
+
+#ADD_DEFINITIONS("-march=armv6")
+add_definitions("-mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard")
+
+# rdynamic means the backtrace should work
+IF (CMAKE_BUILD_TYPE MATCHES "Debug")
+   add_definitions(-rdynamic)
+ENDIF()
+
+# avoids annoying and pointless warnings from gcc
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -U_FORTIFY_SOURCE")
+
